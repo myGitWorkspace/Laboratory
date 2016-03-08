0f589fd0aeca0e94b3c44938dde7f87167ecb059
myGitWorkspace <physuniv14@gmail.com>
2016-03-09 01:37:33 +0200

    Add folder

--- PostgresConnect1/script/pizza_order_system_db.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE pizza (id SERIAL PRIMARY KEY, name VARCHAR(50), type VARCHAR(50),  price REAL);
-CREATE TABLE orders (id SERIAL PRIMARY KEY, customer_id INTEGER, state VARCHAR(50));
-CREATE TABLE customer (id SERIAL PRIMARY KEY, name VARCHAR(50), address VARCHAR(200));
-CREATE TABLE order_pizza (order_id INTEGER, pizza_id INTEGER,  price REAL);
